a8dcfeb82016e71928f634917e1679c944fd25c3
TuringAI2020 <turingai@qq.com>
2019-12-31 00:44:10 +0800

    前端数据库基本完成

--- WangJun.NC.YunDocument.Runner/Program.cs
+++ WangJun.NC.YunDocument.Runner/Program.cs
@@ -21,26 +21,31 @@ namespace WangJun.NC.YunDocument.Runner
             var files = Directory.GetFiles(@"D:\test").ToList();
             var frontDB = FrontDB.Current;
 
-            for (int k = 0; k < files.Count; k++)
-            {
-                var p = files[k];
-                var file = new FileInfo(p);
-                var id = Guid.Parse($"00000000-0000-0000-0000-00000000000{k + 1}");
-                frontDB.Save(JSON.ToJson(new Document
-                {
-                    ItemID= id
-                    ,Id = id
-                    ,Title = file.Name
-                    , Detail = File.ReadAllText(p)
-                    , CreateTime=DateTime.Now
-                    , Status = (int)ENUM.实体状态.正常
-                    , CreatorId = Guid.Empty
-                    , Sort = DateTime.Now.Second
-                })); 
-            }
+            //for (int k = 0; k < files.Count; k++)
+            //{
+            //    var p = files[k];
+            //    var file = new FileInfo(p);
+            //    var id = Guid.Parse($"00000000-0000-0000-0000-00000000000{k + 1}");
+            //    frontDB.Save(JSON.ToJson(new Document
+            //    {
+            //        ItemID= id
+            //        ,Id = id
+            //        ,Title = file.Name
+            //        , Detail = File.ReadAllText(p)
+            //        , CreateTime=DateTime.Now
+            //        , Status = (int)ENUM.实体状态.正常
+            //        , CreatorId = Guid.Empty
+            //        , Sort = DateTime.Now.Second
+            //        , GroupId= id
+            //    })); 
+            //}
 
-            var query = frontDB.QueryList(JSON.ToJson(new QueryFilter { Status = "1" }));
-            
+            //var query1 = frontDB.QueryList(JSON.ToJson(new QueryFilter { Status = "1" }));
+            //var query2 = frontDB.QueryList(JSON.ToJson(new QueryFilter { Keywords = "苹果" }));
+            //var query3 = frontDB.QueryList(JSON.ToJson(new QueryFilter { GroupId = "00000000-0000-0000-0000-000000000003" }));
+            // var query4 = frontDB.QueryList(JSON.ToJson(new QueryFilter { CreatorId = "00000000-0000-0000-0000-000000000000" }));
+
+
             //var classFullName = typeof(Front.LogOperation).FullName;
             //for (int k = 0; k < 100; k++)
             //{
@@ -49,6 +54,35 @@ namespace WangJun.NC.YunDocument.Runner
             //}
 
             //DB.SyncDB<Front.LogOperation,LogOperation>();
+            //var id = Guid.Parse($"00000000-0000-0000-0000-000000000002");
+            //frontDB.Save(JSON.ToJson(new Document
+            //{
+            //    ItemID = id
+            //    ,
+            //    Id = id
+            //    ,
+            //    Title = "更新标题"
+            //    ,
+            //    Detail = "更新内容"
+            //    ,
+            //    CreateTime = DateTime.Now
+            //    ,
+            //    Status = (int)ENUM.实体状态.正常
+            //    ,
+            //    CreatorId = Guid.Empty
+            //    ,
+            //    Sort = DateTime.Now.Second
+            //    ,
+            //    GroupId = id
+            //}));
+
+            var id = Guid.Parse($"00000000-0000-0000-0000-000000000002");
+            frontDB.Remove(JSON.ToJson(new Document
+            {
+                ItemID = id
+                ,
+                Id = id 
+            }));
             Console.WriteLine("OK");
 
 
