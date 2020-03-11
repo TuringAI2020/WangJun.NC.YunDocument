9ef761334ca868c70b213418436f2b9d66315266
TuringAI2020 <turingai@qq.com>
2020-03-12 01:26:56 +0800

    添加腾讯AI接口

---
+++ WangJun.NC.YunStockService.WebAPI/Controllers/YunAIServiceController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WangJun.NC.YunUtils;
+
+namespace WangJun.NC.YunStockService.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class YunAIServiceController : ControllerBase
+    { 
+
+        private readonly ILogger<YunAIServiceController> _logger;
+
+        public YunAIServiceController(ILogger<YunAIServiceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public RES Get(string token,string method, string req)
+        {
+            var inst = AI_T.GetInst();
+            var res = inst.GetType().GetMethod(method).Invoke(inst, new object[] { req });
+            return res as RES;
+        }
+
+        [HttpPost]
+        public RES Post()
+        { 
+            var method = this.Request.Form["method"];
+            var req = this.Request.Form["req"];
+            var inst = AI_T.GetInst();
+            var res = inst.GetType().GetMethod(method).Invoke(inst, new object[] { req.ToString() });
+            return res as RES;
+        }
+    }
+}
